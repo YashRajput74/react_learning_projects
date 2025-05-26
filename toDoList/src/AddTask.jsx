eabb92f92acb980673d909229b47918ac8bddade
Yash Rajput <yashrajput7455@gmail.com>
2025-05-27 01:59:34 +0530

    done Context+Reducer

--- toDoList/src/AddTask.jsx
+++ toDoList/src/AddTask.jsx
@@ -1,8 +1,22 @@
+import { useContext, useState } from "react"
+import { TasksContext } from "./Context";
+let nextId=5;
+
 export default function AddTask(){
+    const {dispatch}=useContext(TasksContext);
+    const [text,setText]=useState('');
     return (
         <>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text" value={text} onChange={e=>setText(e.target.value)}/>
+            <button onClick={()=>{
+                setText('');
+                dispatch({
+                    type:'added',
+                    id:nextId++,
+                    text:text
+                })
+            }
+            }>Add</button>
         </>
 
     )
--- toDoList/src/Context.jsx
+++ toDoList/src/Context.jsx
@@ -1,13 +1,50 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react"
 
 const initialTasks = [
     { id: 1, text: 'Go on a Trip', done: false},
-    { id: 1, text: 'Complete a Project', done: true},
-    { id: 1, text: 'Learn React', done: false},
-    { id: 1, text: 'Attend a meeting', done: false},
+    { id: 2, text: 'Complete a Project', done: true},
+    { id: 3, text: 'Learn React', done: false},
+    { id: 4, text: 'Attend a meeting', done: false},
 ]
 
-export default function TaskProvider() {
-    const TasksContext = createContext(initialTasks);
-    return
+export const TasksContext = createContext(initialTasks);
+export default function TaskProvider({children}) {
+    const [tasks,dispatch]=useReducer(
+        tasksReducer,
+        initialTasks
+    )
+    return (
+        <TasksContext.Provider value={{tasks,dispatch}}>
+            {children}
+        </TasksContext.Provider>
+    )
+}
+
+
+function tasksReducer(tasks,action){
+    switch (action.type){
+        case 'added': {
+            return [...tasks,{
+                id:action.id,
+                text:action.text,
+                done:false
+            }]
+        }
+        case 'changed': {
+            return tasks.map(t=>{
+                if(t.id===action.task.id){
+                    return action.task;
+                }
+                else{
+                    return t;
+                }
+            })
+        }
+        case 'deleted': {
+            return tasks.filter(t=>t.id!==action.id)
+        }
+        default: {
+            throw Error('Unknown action: '+action.type);
+        }
+    }
 }
